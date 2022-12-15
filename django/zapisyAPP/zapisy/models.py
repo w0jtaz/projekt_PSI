2ad58c72a43e1ea707dbb1687a74a2c7dc3708b2
w0jtaz <73688850+w0jtaz@users.noreply.github.com>
2022-12-15 10:16:59 +0100

    zmiany w modelach

--- django/zapisyAPP/zapisy/models.py
+++ django/zapisyAPP/zapisy/models.py
@@ -14,6 +14,8 @@ class klient(models.Model):
     id_klient = models.IntegerField().primary_key
     nazwa = models.CharField(max_length=45, blank=False, null=False)
     email = models.CharField(max_length=45, blank=False, null=False)
+    telefon = models.CharField(max_length=9, blank=False, null=False)
+    adres = models.CharField(max_length=45, blank=False, null=False)
 
 class wydarzenie(models.Model):
     id_zawody = models.IntegerField().primary_key
