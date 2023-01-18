aa0af4f06160d5c54766e4754fbdca642a4d9de0
w0jtaz <73688850+w0jtaz@users.noreply.github.com>
2023-01-19 00:18:56 +0100

    uporządkowanie repozytorium po zmianach w strukturze

---
+++ zapisy/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    pass
+
+
+class ZawodnikForm(forms.Form):
+    imie = forms.CharField()
+    nazwisko = forms.CharField()
+    klub = forms.CharField()
+
+
+class ZawodyForm(forms.Form):
+    nazwa = forms.CharField()
+    # organizator = forms.
+
+
+class WynikiForm(forms.Form):
+    zawodnik = forms.CharField()
+    zawodnik = forms.CharField()
+    dystans = forms.CharField()
+
+
+class ZapisyForm(forms.Form):
+    dystans = forms.CharField()
+
+
+class KlientForm(forms.Form):
+    nazwa = forms.CharField()
+    email = forms.CharField()
+    telefon = forms.CharField()
+    adres = forms.CharField()
